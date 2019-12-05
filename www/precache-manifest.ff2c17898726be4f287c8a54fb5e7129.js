6d32a9091914b939263db90e902102f61724627b
adminV <wenqiang.li@renren-inc.com>
2019-12-05 18:45:54 +0800

    cordova android

---
+++ www/precache-manifest.ff2c17898726be4f287c8a54fb5e7129.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d191438c7ba54656e7",
+    "url": "css/asyncChunk.2633d115.css"
+  },
+  {
+    "revision": "4afcc3337319a050e1a8",
+    "url": "css/chunk-vendors.fa4b6cae.css"
+  },
+  {
+    "revision": "ae863a3d905030d554c5",
+    "url": "css/index.76da585c.css"
+  },
+  {
+    "revision": "c58a434861d65a58b7c71afdbacc72bb",
+    "url": "img/bank_bg_blue.c58a4348.png"
+  },
+  {
+    "revision": "671f096cf668290c3537436f75b896e6",
+    "url": "img/bank_bg_red.671f096c.png"
+  },
+  {
+    "revision": "894c275e0b67cb451b9a2a87612b8281",
+    "url": "img/banner/banner.png"
+  },
+  {
+    "revision": "9c53d6c3d2746f1c65d896b1435d3e2a",
+    "url": "img/bg_login.9c53d6c3.png"
+  },
+  {
+    "revision": "c99f87afb66d184ce38cff19b03b18ce",
+    "url": "img/bg_myinfo.c99f87af.png"
+  },
+  {
+    "revision": "7e222b536182bf8f85e32a7f1eff2407",
+    "url": "img/bg_wealth.7e222b53.png"
+  },
+  {
+    "revision": "ac7ba65297730c70187a36e756546413",
+    "url": "img/credit-1.ac7ba652.png"
+  },
+  {
+    "revision": "f7f1dade0624797ecea1fbd248381ea4",
+    "url": "img/credit-2.f7f1dade.png"
+  },
+  {
+    "revision": "0ddf2747a386853c53dc864b2cb983a8",
+    "url": "img/credit-bg.0ddf2747.png"
+  },
+  {
+    "revision": "c69250881c4d080a3722a2c46efdcfe0",
+    "url": "img/defaultPhoto.png"
+  },
+  {
+    "revision": "e581f1936e797434541e52d6ace80982",
+    "url": "img/examine_watting.e581f193.png"
+  },
+  {
+    "revision": "2384df07aa73bcaa6fd616ea0fd2de57",
+    "url": "img/fail_icon.2384df07.png"
+  },
+  {
+    "revision": "2adb592ef6300249f4387f8c5522bb0b",
+    "url": "img/login_icon.2adb592e.png"
+  },
+  {
+    "revision": "996a5d24891469c5030a197419368a0a",
+    "url": "img/nobill.996a5d24.png"
+  },
+  {
+    "revision": "f6056d078e1c6bf6f8aecb534c46fbaf",
+    "url": "img/success_icon.f6056d07.png"
+  },
+  {
+    "revision": "0635ee89180e85afb3fe0c71d107f46b",
+    "url": "index.html"
+  },
+  {
+    "revision": "a7d191438c7ba54656e7",
+    "url": "js/asyncChunk.77f89990.js"
+  },
+  {
+    "revision": "4afcc3337319a050e1a8",
+    "url": "js/chunk-vendors.33370299.js"
+  },
+  {
+    "revision": "ae863a3d905030d554c5",
+    "url": "js/index.22393ccd.js"
+  },
+  {
+    "revision": "4ea5e5806a765d92a0ca4fd76b4946ce",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "static/echarts-all.js"
+  }
+]);
